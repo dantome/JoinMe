993b86ad99d945655dc9ff133e96f94edc0c1958
Dan Tome <dtome00@gmail.com>
2019-05-11 00:11:49 -0400

    Created new MyGroups class

---
+++ joinMeApp/src/components/MyGroups.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class MyGroups extends React.Component {
+    render () {
+        return (
+            <View>
+                <Text>
+                    These are my groups!
+                </Text>
+            </View>
+        )
+    }
+}
